d939caed978d319109c53035f2ff5e6a94678b55
Prince Singh <prince@xoogle.in>
2021-03-24 11:45:38 +0530

    img added

--- src/Components/SalesforceProducts/SalesforceProducts.jsx
+++ src/Components/SalesforceProducts/SalesforceProducts.jsx
@@ -1,5 +1,6 @@
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import Sales from '../Assests/img/sales.png'
 import "./sales.css";
 
 function SalesforeProducts() {
@@ -23,7 +24,10 @@ function SalesforeProducts() {
         <div class="container">
           <div class="row">
             <div class="col-lg-4 p-lg-3">
-              <h3><span>Sales Cloud</span></h3>
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>
+                <span>Sales Cloud</span>
+              </h3>
               <div class="content">
                 <ul>
                   <li>Accelerate your business growth by selling smarter.</li>
@@ -44,7 +48,10 @@ function SalesforeProducts() {
               </div>
             </div>
             <div class="col-lg-4 p-lg-3">
-              <h3><span>Service Cloud</span></h3>
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>
+                <span>Service Cloud</span>
+              </h3>
               <div class="content">
                 <ul>
                   <li>Deliver personalized service.</li>
@@ -70,7 +77,10 @@ function SalesforeProducts() {
               </div>
             </div>
             <div class="col-lg-4 p-lg-3">
-              <h3><span>Marketing Cloud</span></h3>
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>
+                <span>Marketing Cloud</span>
+              </h3>
               <div class="content">
                 <ul>
                   <li>Engage with your customer with meaningful messages.</li>
@@ -95,64 +105,137 @@ function SalesforeProducts() {
         <div class="container">
           <div class="row">
             <div class="col-lg-4 p-lg-3">
-              <h3><span>Community Cloud</span></h3>
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>
+                <span>Community Cloud</span>
+              </h3>
               <div class="content">
                 <ul>
                   <li>Personalized connect with your customers via customer</li>
+                  <li>Listen to your partners voice via partner community.</li>
                   <li>
-                  Listen to your partners voice via partner community.
+                    Drive employee satisfaction via self-service portals built
+                    on employee communities.
                   </li>
-                  <li>
-                  Drive employee satisfaction via self-service portals built on employee communities.
-                  </li>
-                  <li>
-                  Build and publish lightening quick applications.
-                  </li>
-               
+                  <li>Build and publish lightening quick applications.</li>
                 </ul>
               </div>
             </div>
             <div class="col-lg-4 p-lg-3">
-              <h3><span>Salesforce Platform</span></h3>
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>
+                <span>Salesforce Platform</span>
+              </h3>
               <div class="content">
                 <ul>
                   <li>Deliver personalized service.</li>
-                  <li>
-                  Create blazing fast business specific applications.
-                  </li>
+                  <li>Create blazing fast business specific applications.</li>
                   <li>Metadata driven architecture.</li>
-                  <li>
-                  Drag and drop workflows.
-                  </li>
-                  <li>
-                  Create customer facing application using Heroku.
-                  </li>
+                  <li>Drag and drop workflows.</li>
+                  <li>Create customer facing application using Heroku.</li>
                   <li>Mobile, Vision, Blockchain, AI etc.</li>
-                  <li>
-                  Extend your CRM’s reach and functionality.
-                  </li>
+                  <li>Extend your CRM’s reach and functionality.</li>
                 </ul>
               </div>
             </div>
             <div class="col-lg-4 p-lg-3">
-              <h3><span>Analytics</span></h3>
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>
+                <span>Analytics</span>
+              </h3>
               <div class="content">
                 <ul>
                   <li>Access your data across the enterprise.</li>
+                  <li>Create stories out of data.</li>
                   <li>
-                  Create stories out of data.
+                    Get the complete control of your business over your mobile.
                   </li>
-                  <li>
-                  Get the complete control of your business over your mobile.
-                  </li>
-             
                 </ul>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <Footer/>
+      <div class="container-fluid mb-5" id="product4">
+        <div class="container">
+          <h1>
+            Our <span>Consulting</span> Services
+          </h1>
+          <div class="row mt-5">
+            <div class="col-lg-4">
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>Process Consulting</h3>
+              <p>
+                Our highly valued Consulting services expertise, across
+                industries, empower our customers in their Business Automation &
+                Business Process optimisation goals.If one or more of the above
+                questions you just read has “Yes” as an answer, then it’s time
+                to engage our services. Whatever the problem is, it is of utmost
+                importance to find the right solution. Get expert advice from
+                our process consultants.
+              </p>
+            </div>
+            <div class="col-lg-4">
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>Salesforce Product Implementation</h3>
+              <p>
+                We help customers across industry verticals in redefining their
+                sales, marketing and customer service experience using
+                Salesforce products.We have certified and experienced team of
+                Salesforce professionals who leverage extensive expertise in
+                Salesforce configuration and customization to empower
+                enterprises in their digital transformation journey.
+              </p>
+            </div>
+            <div class="col-lg-4">
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>System Integration</h3>
+              <p>
+                Our team of Integration architects help our customers to get
+                seamless connectivity between Salesforce and various legacy and
+                home-grown systems, in real time as well as batch mode.Data
+                plays a crucial role and our experts assist our customers in
+                accurately mapping it in the system to provide clear visibility
+                into the business, leading to informed decision making.
+              </p>
+            </div>
+          </div>
+          <div class="row mt-3">
+            <div class="col-lg-4">
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>Audit & Optimization</h3>
+              <p>
+                Our team of technical architects helps enterprises in assessing
+                their current architecture and defining the optimized
+                transformation strategy.The audit is backed up by an exhaustive
+                report and the recommendation and plan to optimize the current
+                setup on the process as well as architecture level.
+              </p>
+            </div>
+            <div class="col-lg-4">
+            <img  src={Sales} alt="Quadrafort" class="img-fluid"/>
+              <h3>Support & Adoption</h3>
+              <p>
+                The success of any implementation depends on its adoption. We
+                have a dedicated team engaged in support & training, which helps
+                in driving system adoption.We support customer via following
+                methodologies:
+              </p>
+              <ul class="ml-4">
+                <li>SLA Based support</li>
+                <li>
+                  Dedicated resource – T&M model We provide following sets of
+                  training to the customer’s team:
+                </li>
+                <li>User training</li>
+                <li>Admin training</li>
+                <li>Module wise training</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 }
